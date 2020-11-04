5fb78e89e58f2907cd8ebb09f9ff79573147e15c
Yoichi Kobayashi <info@tplh.net>
2020-11-04 17:13:52 +0900

    fixed the wrong variable name.

--- hook.glsl
+++ hook.glsl
@@ -3,7 +3,7 @@
 #endif
 
 vec3 hook(vec3 velocity, vec3 anchor, float restLength, float k) {
-  return normalize(velocity - anchor + PRECISION) * (-1.0 * k * (length(v - anchor) - restLength));
+  return normalize(velocity - anchor + PRECISION) * (-1.0 * k * (length(velocity - anchor) - restLength));
 }
 
 #pragma glslify: export(hook)
